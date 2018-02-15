e10e32d46344f0828b8593311e8bd9012df8ee87
Marcio Gasparotto <mgasparotto@gmail.com>
2018-02-15 17:42:30 -0200

    add store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { store, view } from 'react-easy-state';
+
+let state = store({
+  charts: ['A', 'B', 'C'],
+  selectedChart: null,
+});
 
 class Kowalski extends Component {
   constructor(props) {
     super(props);
   }
 
+  changeSelected = ev => {
+    state.selectedChart = ev.target.value;
+  };
+
   render() {
+    const buttons = state.charts.map(chart => (
+      <button onClick={this.changeSelected} value={chart} key={`chart-${chart}`}>
+        {chart}
+      </button>
+    ));
+
     return (
       <div>
         <h2>{this.props.appName}</h2>
+        <h3>Selected Chart: {state.selectedChart}</h3>
+        <hr />
+        {buttons}
       </div>
     );
   }
@@ -19,4 +38,4 @@ Kowalski.propTypes = {
   appName: PropTypes.string.isRequired,
 };
 
-export default Kowalski;
+export default view(Kowalski);
