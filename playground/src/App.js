e3e21a688c81bdcc28413d1c10df5c8497462e9d
Marcio Gasparotto <mgasparotto@gmail.com>
2018-05-08 12:51:12 -0300

    wip

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,155 +1,53 @@
 import React, { Component } from 'react';
 import Kowalski from 'kowalski';
-// import axios from 'axios';
 import SkyLight from 'react-skylight';
 import './App.css';
 import ReactEcharts from 'echarts-for-react';
 import echarts from 'echarts';
 
-// const schema = [
-//   { columnName: 'id', columnLabel: 'Id', columnType: 'numeric' },
-//   { columnName: 'responsavel', columnLabel: 'Responsável', columnType: 'varchar' },
-//   { columnName: 'supervisor', columnLabel: 'Supervisor', columnType: 'varchar' },
-//   { columnName: 'estado', columnLabel: 'UF', columnType: 'varchar' },
-//   { columnName: 'status', columnLabel: 'Status', columnType: 'varchar' },
-//   { columnName: 'estabelecimento', columnLabel: 'Estabelecimento', columnType: 'varchar' },
-//   { columnName: 'obrigacao', columnLabel: 'Ogrigação', columnType: 'varchar' },
-//   { columnName: 'valorApurado', columnLabel: 'Valor Apurado', columnType: 'numeric' },
-//   { columnName: 'valorPago', columnLabel: 'Valor Pago', columnType: 'numeric' },
-//   { columnName: 'dataPagamento', columnLabel: 'Dt. Pagamento', columnType: 'timestamp' },
-//   { columnName: 'dataVencimento', columnLabel: 'Dt. Vencimento', columnType: 'timestamp' },
-// ];
+import chart1 from './chart1';
+import chart2 from './chart2';
+import chart3 from './chart3';
+import chart4 from './chart4';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      chartOpts: {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            lineStyle: {
-              color: '#ccc',
-            },
-          },
-          backgroundColor: 'rgba(255,255,255,1)',
-          padding: [5, 10],
-          textStyle: {
-            color: 'rgba(76,106,148,1)',
-          },
-          extraCssText: 'box-shadow: 0 0 5px rgba(0,0,0,0.3)',
-        },
-        xAxis: {
-          type: 'category',
-          data: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
-          boundaryGap: true,
-          position: 'bottom',
-          axisLine: {
-            show: true,
-            lineStyle: {
-              color: 'rgba(100,100,100,1)',
-            },
-          },
-          axisTick: {
-            show: false,
-          },
-          splitLine: {
-            show: false,
-          },
-          axisLabel: {
-            show: true,
-            rotate: 0,
-            color: 'rgba(33,33,33, 1)',
-            margin: '8',
-            padding: [10, 5],
-          },
-        },
-        yAxis: {
-          type: 'value',
-          position: 'left',
-          axisLine: {
-            show: true,
-            lineStyle: {
-              color: 'rgba(100,100,100,1)',
-            },
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            show: true,
-            rotate: 0,
-            color: 'rgba(33,33,33, 1)',
-            // margin: '8',
-            // padding: [0, 0],
-          },
-          splitLine: {
-            show: false,
-          },
-        },
-        series: [
-          {
-            data: [820, 232, 901, 434, 1290, 2330, 754],
-            type: 'line',
-            itemStyle: {
-              color: 'rgba(68, 202, 104, 1)',
-            },
-            name: 'Crédito',
-            lineStyle: {
-              width: 3,
-            },
-            smooth: false,
-            markPoint: {
-              data: [{ type: 'max', name: 'Máximo' }, { type: 'min', name: 'Mínimo' }],
-            },
-            label: {
-              show: true,
-              position: [0, 15],
-              shadowColor: 'rgba(30,30,30,.8)',
-              shadowBlur: 2,
-              shadowOffsetX: 1,
-              shadowOffsety: 1,
-            },
-          },
-          {
-            data: [240, 722, 1801, 734, 510, 1130, 321],
-            type: 'bar',
-            barWidth: '80%',
-            itemStyle: {
-              color: 'rgba(255,0,17, 1)',
-            },
-            name: 'Débito',
-            label: {
-              normal: {
-                show: true,
-                position: 'insideTop',
-              },
-            },
-            // lineStyle: {
-            //   width: 3,
-            // },
-            // smooth: true,
-          },
-        ],
-      },
+      chart1: chart1,
+      chart2: chart2,
+      chart3: chart3,
+      chart4: chart4,
+      currentChart: null,
     };
     this.updateChart = this.updateChart.bind(this);
+    this.editChart = this.editChart.bind(this);
+    this.getChartOptions = this.getChartOptions.bind(this);
   }
 
-  componentDidMount() {
-    // console.time('lOAD-TIME');
-    // axios.get('http://localhost:8080/stats/data?records=1M').then(response => {
-    //   console.timeEnd('lOAD-TIME');
-    //   console.time('SETSTATE-TIME');
-    //   this.setState({ data: response.data.data }, () => console.timeEnd('SETSTATE-TIME'));
-    // });
-  }
+  componentDidMount() {}
 
-  updateChart(newChartOpts) {
+  editChart(chartId) {
+    let newState = this.state;
+    newState.currentChart = chartId;
     debugger;
-    this.setState({ chartOpts: newChartOpts });
-    this.simpleDialog.hide();
+    this.setState(newState, () => {
+      setTimeout(() => {
+        debugger;
+        this.simpleDialog.show();
+      }, 200);
+    });
+  }
+
+  getChartOptions() {
+    debugger;
+    return Object.assign({}, this.state[this.state.currentChart]);
+  }
+
+  updateChart(newchart) {
+    let newState = this.state;
+    newState[this.state.currentChart] = newchart;
+    this.setState(newState, () => this.simpleDialog.hide());
   }
 
   render() {
@@ -170,21 +68,56 @@ class App extends Component {
     return (
       <div className="app">
         <section style={{ textAlign: 'center' }}>
-          <ReactEcharts
-            notMerge={true}
-            option={this.state.chartOpts}
-            echarts={echarts}
-            style={{ height: '600px', width: '700px', margin: '0 auto' }}
-          />
-          <button onClick={() => this.simpleDialog.show()}>Opções do Gráfico</button>
+          <div style={{ width: '49%', float: 'left' }}>
+            <ReactEcharts
+              notMerge={true}
+              option={this.state.chart1}
+              echarts={echarts}
+              style={{ height: '400px', margin: '0 auto' }}
+            />
+            <button onClick={() => this.editChart('chart1')}>Opções do Gráfico</button>
+          </div>
+          <div style={{ width: '49%', float: 'right' }}>
+            <ReactEcharts
+              notMerge={true}
+              option={this.state.chart2}
+              echarts={echarts}
+              style={{ height: '400px', margin: '0 auto' }}
+            />
+            <button onClick={() => this.editChart('chart2')}>Opções do Gráfico</button>
+          </div>
+        </section>
+        <section style={{ textAlign: 'center' }}>
+          <div style={{ width: '49%', float: 'left' }}>
+            <ReactEcharts
+              notMerge={true}
+              option={this.state.chart3}
+              echarts={echarts}
+              style={{ height: '400px', margin: '0 auto' }}
+            />
+            <button onClick={() => this.editChart('chart3')}>Opções do Gráfico</button>
+          </div>
+          <div style={{ width: '49%', float: 'right' }}>
+            <ReactEcharts
+              notMerge={true}
+              option={this.state.chart4}
+              echarts={echarts}
+              style={{ height: '400px', margin: '0 auto' }}
+            />
+            <button onClick={() => this.editChart('chart4')}>Opções do Gráfico</button>
+          </div>
         </section>
         <SkyLight dialogStyles={myBigDialog} ref={ref => (this.simpleDialog = ref)}>
-          <Kowalski
-            appName="Kowalski Playground"
-            chart={this.state.chartOpts}
-            containerHeight={modalHeight}
-            onFinish={this.updateChart}
-          />
+          {this.state.currentChart ? (
+            <Kowalski
+              appName="Kowalski Playground"
+              chart={this.getChartOptions()}
+              containerHeight={modalHeight}
+              onFinish={this.updateChart}
+            />
+          ) : (
+            ''
+          )}
         </SkyLight>
       </div>
     );
---
+++ playground/src/chart1.js
@@ -0,0 +1,103 @@
+export default {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      lineStyle: {
+        color: '#ccc',
+      },
+    },
+    backgroundColor: 'rgba(255,255,255,1)',
+    padding: [5, 10],
+    textStyle: {
+      color: 'rgba(76,106,148,1)',
+    },
+    extraCssText: 'box-shadow: 0 0 5px rgba(0,0,0,0.3)',
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
+    boundaryGap: true,
+    position: 'bottom',
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: 'rgba(100,100,100,1)',
+      },
+    },
+    axisTick: {
+      show: false,
+    },
+    splitLine: {
+      show: false,
+    },
+    axisLabel: {
+      show: true,
+      rotate: 0,
+      color: 'rgba(33,33,33, 1)',
+      margin: '8',
+      padding: [10, 5],
+    },
+  },
+  yAxis: {
+    type: 'value',
+    position: 'left',
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: 'rgba(100,100,100,1)',
+      },
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      show: true,
+      rotate: 0,
+      color: 'rgba(33,33,33, 1)',
+    },
+    splitLine: {
+      show: false,
+    },
+  },
+  series: [
+    {
+      data: [820, 232, 901, 434, 1290, 2330, 754],
+      type: 'line',
+      itemStyle: {
+        color: 'rgba(68, 202, 104, 1)',
+      },
+      name: 'Crédito',
+      lineStyle: {
+        width: 3,
+      },
+      smooth: false,
+      markPoint: {
+        data: [{ type: 'max', name: 'Máximo' }, { type: 'min', name: 'Mínimo' }],
+      },
+      label: {
+        show: true,
+        position: [0, 15],
+        shadowColor: 'rgba(30,30,30,.8)',
+        shadowBlur: 2,
+        shadowOffsetX: 1,
+        shadowOffsety: 1,
+      },
+    },
+    {
+      data: [240, 722, 1801, 734, 510, 1130, 321],
+      type: 'bar',
+      barWidth: '80%',
+      itemStyle: {
+        color: 'rgba(255,0,17, 1)',
+      },
+      name: 'Débito',
+      label: {
+        normal: {
+          show: true,
+          position: 'insideTop',
+        },
+      },
+    },
+  ],
+};
